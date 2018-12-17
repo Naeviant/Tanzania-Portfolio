7d6c6e12ec5cdd4ab399c0af451f741b5037f0bb
Sam Hirst <45145746+SamuelJHirst@users.noreply.github.com>
2018-12-17 20:40:01 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require("express"),
+    config = require("./config"),
+    bodyParser = require("body-parser"),
+    app = express(),
+    session = require("express-session"),
+    cookieParser = require("cookie-parser"),
+    swig = require("swig");
+
+app.engine("html", swig.renderFile);
+app.set("view engine", "html");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/static"));
+app.use(cookieParser());
+app.use(session({secret: "anything", resave: false, saveUninitialized: false}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view cache", false);
+swig.setDefaults({cache: false});
+
+app.locals = {
+    
+};
+
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+app.get("/fundraising/", function(req, res) {
+    res.render("fundraising");
+});
+
+app.get("/gallery/", function(req, res) {
+    res.render("gallery");
+});
+
+app.get("/sponsors/", function(req, res) {
+    res.render("sponsors");
+});
+
+var server = app.listen(config.app.port, function() {
+    console.log("Listening on Port " + config.app.port);
+});
